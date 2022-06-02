0304fa916ef5b092adfc2216a7e905f8bd8d1987
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-06-02 22:02:34 +0630

    install, activate, fetch

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-const registerServiceWorker = async () => {
+window.addEventListener("load", start);
+
+function start() {
   if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register(
-        "/service-workers/sw.js",
-        {
-          scope: "/service-workers/",
-        }
-      );
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker waiting");
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
-    } catch (error) {
-        console.log('Registration filled with ' + error);
-    }
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("service worker registered"))
+      .catch(() => console.log("Failed to register service worker"));
   }
-};
-
-
-registerServiceWorker();
+}
--- sw.js
+++ sw.js
@@ -1,71 +1,27 @@
-
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1");
-  await cache.addAll(resources);
-};
-
-const putInCache = async (request, response) => {
-  const cache = await caches.open("v1");
-  await cache.put(request, response);
-};
-
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-
+async function addResource(resources) {
   try {
-    const responseFromNetwork = await fetch(request);
-
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
   } catch (error) {
-    const fallbackResponse = await caches.match(fallbackUrl);
-    if (fallbackResponse) {
-      return fallbackResponse;
-    }
-
-    return new Response("Network error happened", {
-      status: 408,
-      headers: { "Content-Type": "text/plain" },
-    });
+    console.log(error);
+    throw error;
   }
-};
+}
 
-const enableNavigationPreload = async () => {
-  if (self.registration.navigationPreload) {
-    await self.registration.navigationPreload.enable();
-  }
-};
-
-self.addEventListener("activate", (event) =>
-  event.waitUntil(enableNavigationPreload())
-);
-
-self.addEventListener("install", (event) => {
+self.addEventListener("install", async (event) => {
   event.waitUntil(
-    addResourcesToCache([
-      "/service-workers/",
-      "/service-workers/index.html",
-      "/service-workers/style.css",
-      "/service-workers/app.js",
-      "/service-workers/gallery/irene-1.jpg",
-      "/service-workers/gallery/irene-2.jpg",
-      "/service-workers/gallery/irene-3.jpeg",
-      "/service-workers/go-4.jpeg",
+    addResource([
+      "/index.html",
+      "/style.css",
+      "/img-list.js",
+      "/app.js",
+      "/gallery/irene-1.jpg",
+      "/gallery/irene-2.jpg",
+      "/gallery/irene-4.jpeg",
     ])
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(
-      cacheFirst({
-        request: event.request,
-        preloadResponsePromise: event.preloadResponse,
-        fallbackUrl: "/service-workers/gallery/irene-1.jpg",
-      })
-    )
-  );
+self.addEventListener("activate", (event) => {
+  console.log("sw activate ", event);
 });
