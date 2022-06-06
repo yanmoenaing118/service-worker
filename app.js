e58ebf061455f695dd96d14e2758b628adceef97
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-06-06 23:54:39 +0630

    sadd ply

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-// window.addEventListener("load", start);
+window.addEventListener("load", start);
 
 
-// function start() {
-//   if ("serviceWorker" in navigator) {
+function start() {
 
-//     registerServiceWorker();
+  if ("serviceWorker" in navigator) {
 
-//   }
-// }
+    registerServiceWorker();
 
-// function registerServiceWorker() {
-//   navigator.serviceWorker
-//     .register("/sw.js")
-//     .then(() => console.log("Service worker installed"))
-//     .catch(() => console.log("Sevice Worker discarded"));
-// }
+  }
+}
+
+function registerServiceWorker() {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(() => console.log("Service worker installed"))
+    .catch(() => console.log("Sevice Worker discarded"));
+}
+
+
--- sw.js
+++ sw.js
@@ -1,40 +1,42 @@
-// const resources = [
-//   "/index.html",
-//   "/go-4.jpeg",
-//   "/gallery/irene-1.jpg",
-//   "/gallery/irene-2.jpg",
-//   "/gallery/irene-4.jpeg",
-//   "/app.js",
-//   "/style.css",
-// ];
+const resources = [
+  "/index.html",
+  "/gallery/irene-1.jpg",
+  "/gallery/irene-2.jpg",
+  "/gallery/irene-4.jpeg",
+  "/gallery/go-4.jpeg",
+  "/gallery/wendy.jpg",
+  "/app.js",
+  "/style.css",
+  "/mp3/psycho.mp3"
+];
 
-// async function addResourcesToServiceWorker(resources) {
-//   const cache = await caches.open("v1");
+async function addResourcesToServiceWorker(resources) {
+  const cache = await caches.open("v1");
 
-//   await cache.addAll(resources);
-// }
+  await cache.addAll(resources);
+}
 
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(addResourcesToServiceWorker(resources));
-// });
+self.addEventListener("install", (event) => {
+  event.waitUntil(addResourcesToServiceWorker(resources));
+});
 
-// self.addEventListener("activate", (event) => {
-//   console.log("Service worker is handling request");
-// });
+self.addEventListener("activate", (event) => {
+  console.log("Service worker is handling request");
+});
 
-// self.addEventListener("fetch", (event) => {
-//   if (event.request.method != "GET") return;
+self.addEventListener("fetch", (event) => {
+  if (event.request.method != "GET") return;
 
-//   event.respondWith(getResourcesFromCache(event));
-// });
+  event.respondWith(getResourcesFromCache(event));
+});
 
-// async function getResourcesFromCache(e) {
-//   console.log(e.request);
-//   const cache = await caches.open("v1");
-//   const cachedResponse = await cache.match(e.request);
-//   if (cachedResponse) {
-//     e.waitUntil(cache.add(e.request));
-//     return cachedResponse;
-//   }
-//   return fetch(e.request);
-// }
+async function getResourcesFromCache(e) {
+  console.log(e.request);
+  const cache = await caches.open("v1");
+  const cachedResponse = await cache.match(e.request);
+  if (cachedResponse) {
+    e.waitUntil(cache.add(e.request));
+    return cachedResponse;
+  }
+  return fetch(e.request);
+}
